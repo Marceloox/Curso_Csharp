5e88678c0210fdc30bca2750d1f8be1693201eee
Marcelo <marcelo06ferr@gmail.com>
2025-01-16 21:41:42 -0300

    Nova atualização no Exercício.

--- Ex-007/Program.cs
+++ Ex-007/Program.cs
@@ -7,14 +7,20 @@
             double nota_final = 50;
             string resultado;
 
-            if (nota_final >= 60)
-            {
-                resultado = "Aprovado";
-            }else if (nota_final >= 40)
-            {
-                resultado = "Recuperação";
+            if (nota_final >= 60){
+                if (nota_final >= 90){
+                    resultado = "Aprovado com louvor";
+                }
+                else{
+                    resultado = "aprovado";
+                }
             }else {
-                resultado = "Reprovado";
+                if (nota_final >= 40){
+                    resultado = "Recuperação";
+                }
+                else{
+                    resultado = "Reprovado";
+                }
             }
 
             Console.WriteLine("O aluno foi {0}.", resultado);   
