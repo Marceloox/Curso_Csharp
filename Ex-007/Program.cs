64a239959fe11c2fd1b33061190ee7215c538b81
Marcelo <marcelo06ferr@gmail.com>
2025-01-16 21:06:53 -0300

    Atualização do exercício.

---
+++ Ex-007/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex_007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 70;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "Aprovado";
+            }else {
+                resultado = "Reprovado";
+            }
+
+            Console.WriteLine("O aluno foi {0}.", resultado);   
+
+            
+        }
+    }
+}
