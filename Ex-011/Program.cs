ef56730ce29849160392c2e252e2bb8ff53ae953
Marcelo <marcelo06ferr@gmail.com>
2025-01-23 19:33:38 -0300

    Exercício de for.

---
+++ Ex-011/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex_011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double casa, salario, anos, prestação;
+            
+
+            Console.Write("Digite o valor da casa: ");
+            
+        }
+    }
+}
---
+++ Ex-012/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+           Console.WriteLine("Feliz Ano Novo!");
+        }
+    }
+}
