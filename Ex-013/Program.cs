ccd8f1caa1f75a86de84ac45b6a85eba0d92ecb1
Marcelo <marcelo06ferr@gmail.com>
2025-01-23 20:03:39 -0300

    Novo Exercício de for.

---
+++ Ex-013/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int final;
+
+            Console.WriteLine("Digite o limite:");
+            final = Convert.ToInt32(Console.ReadLine());    
+
+            for (int i = 1; i <= final; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0}, ", i);
+                }    
+            }
+        }
+    }
+}
