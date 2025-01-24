34c9d4a03f44dc7badd36a0ce47a26f4d3f90cb2
Marcelo <marcelo06ferr@gmail.com>
2025-01-23 21:14:47 -0300

    Tabuada com for.

---
+++ Ex-015/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = numero * i;
+               Console.WriteLine("{0} x {1} = {2}.", numero, i, resultado);
+              
+            }
+        }
+    }
+}
