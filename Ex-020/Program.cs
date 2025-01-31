03d75cf79775edaa7f82f6910bdb4e3b80e9a28d
Marcelo <marcelo06ferr@gmail.com>
2025-01-30 21:33:35 -0300

    Triângulo.

---
+++ Ex-020/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Calcule a área do triângulo.");
+            Console.WriteLine();
+
+            Console.Write("Digite a base do triângulo (m): ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaTriangulo(largura, 5);
+        }
+
+        static void AreaTriangulo(double b, double h)
+        {
+            double a = (b * h) / 2;
+            Console.WriteLine($"A área do triângulo é de {b}m x {h}m é de {a:f2}m².");
+        }
+    }
+}
