ac052e8100bd24b287ee3889b617368534b9cfd4
Marcelo <marcelo06ferr@gmail.com>
2025-01-30 21:09:36 -0300

    Área do Terreno

---
+++ CalculoÁrea/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CalculoÁrea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Controle de Terrenos.");
+            Console.WriteLine();
+
+            Console.Write("Digite a largura do terreno (m): ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite o comprimento do terreno (m): ");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaTerreno(largura, comprimento);
+        }
+
+        static void AreaTerreno(double l, double c)
+        {
+            double a = l * c;
+            Console.WriteLine($"A área do terreno é de {l}m x {c}m é de {a:f2}m².");
+        }
+    }
+}
